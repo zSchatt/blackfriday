bd1d258a6547a510d431075cf9edc869acac3da7
zSchatt <72706376+zSchatt@users.noreply.github.com>
2021-11-30 09:47:48 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,34 +4,31 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
 
-def busca_iphone():
-    option = Options()
-    option.headless = True
-    driver = webdriver.Firefox()
-    
-    url_amazon = 'https://www.amazon.com.br/Eletronicos-e-Tecnologia/b/?ie=UTF8&node=16209062011&ref_=nav_cs_electronics_aa977060142142808a563d1c6c6b2474'
-    driver.get(url_amazon)
-    sleep(1)
-    driver.find_elements(By.CSS_SELECTOR, 'li.a-spacing-micro.apb-browse-refinements-indent-2')[4].click()
-    #APPLE
-    driver.find_elements(By.CSS_SELECTOR, 'div.bxc-grid__column.bxc-grid__column--3-of-12.bxc-grid__column--light')[0].click()
-    sleep(1)
-    driver.find_elements(By.CSS_SELECTOR, 'span.a-size-base.a-color-base')[8].click()
-    sleep(1)
-    driver.find_element(By.CLASS_NAME, 'a-button-inner').click()
-    driver.find_element(By.ID, 's-result-sort-select_2').click()
-    sleep(1)
-    celulares = driver.find_elements(By.CLASS_NAME, 'sg-col-inner')
-        
-    for i in range(4, 28):
-        sleep(1)
-        cel = celulares[i].text
-        if '$' in celulares[i].text:
-            pesquisa = re.findall('((i|I)(Phone|phone|PHONE)(\s\d+|\d+))', cel)
-            
-            print(pesquisa)
-            
-        i += 1
-        driver.close()
+option = Options()
+option.headless = True
+driver = webdriver.Firefox()
 
-busca_amazon()
+url_amazon = 'https://www.amazon.com.br/Eletronicos-e-Tecnologia/b/?ie=UTF8&node=16209062011&ref_=nav_cs_electronics_aa977060142142808a563d1c6c6b2474'
+driver.get(url_amazon)
+sleep(1)
+driver.find_elements(By.CSS_SELECTOR, 'li.a-spacing-micro.apb-browse-refinements-indent-2')[4].click()
+#APPLE
+driver.find_elements(By.CSS_SELECTOR, 'div.bxc-grid__column.bxc-grid__column--3-of-12.bxc-grid__column--light')[0].click()
+sleep(1)
+driver.find_elements(By.CSS_SELECTOR, 'span.a-size-base.a-color-base')[8].click()
+sleep(1)
+driver.find_element(By.CLASS_NAME, 'a-button-inner').click()
+driver.find_element(By.ID, 's-result-sort-select_2').click()
+sleep(1)
+celulares = driver.find_elements(By.CLASS_NAME, 'sg-col-inner')
+
+for i in range(4, 28):
+    sleep(1)
+    cel = celulares[i].text
+    if '$' in celulares[i].text:
+        pesquisa = re.findall('((i|I)(Phone|phone|PHONE)(\s\d+|\d+))', cel)
+
+        print(pesquisa)
+
+    i += 1
+    driver.close()
